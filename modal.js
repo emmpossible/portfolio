2c42a342cde46d8b686f4075f609eb388ebf7f84
Emily <iemmmueller@gmail.com>
2019-03-01 16:02:09 -0600

    Initial commit
    Initial commit of my website portfolio.

---
+++ modal.js
@@ -0,0 +1,23 @@
+const portfolioContainer = document.querySelector('.portfolio-items')
+
+portfolioContainer.addEventListener('click', e => {
+    e.preventDefault()
+    
+    const modalToggle = e.target.closest('.portfolio-link')
+    if (!modalToggle) return
+    
+    const modal = modalToggle.parentNode.nextElementSibling
+    const closeButton = modal.querySelector('.modal-close')
+    
+    modal.classList.add('is-open')
+    
+    closeButton.addEventListener('click', _ => {
+     modal.classList.remove('is-open')   
+    })
+    
+    document.addEventListener('keydown', e => {
+        if ( e.keyCode === 27 ) {
+            modal.classList.remove('is-open')   
+        }
+    })
+})
